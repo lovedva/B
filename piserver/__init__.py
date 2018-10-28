0e023ecb2942f749fbb35d90e46ace6b3a33c649
Ji Yi <xixixi_0987@hotmail.com>
2018-10-28 23:18:44 +0000

    1

--- piserver/__init__.py
+++ piserver/__init__.py
@@ -42,4 +42,5 @@ def create_app(test_config=None):
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
 
+
     return app
--- piserver/blog.py
+++ piserver/blog.py
@@ -14,11 +14,11 @@ bp = Blueprint('blog', __name__)
 
 @bp.route('/')
 def index():
-        db = get_db()
-        posts = db.execute(
-                    'SELECT p.id, title, body, created, author_id, username'
-                    ' FROM post p JOIN user u ON p.author_id = u.id'
-                    ' ORDER BY created DESC'
-                
-        ).fetchall()
-            return render_template('blog/index.html', posts=posts)
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
