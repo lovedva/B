8b7dc9584ffd3bb3636ac96fbe3ee212b3f4f3b3
Ji Yi <xixixi_0987@hotmail.com>
2018-11-07 03:43:46 +0000

    1

--- piserver/picp.py
+++ piserver/picp.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+#from scripts import digital_re
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
+
 bp = Blueprint('picp', __name__ ,url_prefix='/picp')
 
 @bp.route('/')
 def index():
+	print ("进入picp，index()方法")
 	return render_template('index.html')
-#	print ("进入 picp-index ")
