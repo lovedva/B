4bfe129c439a12c8296f22af0c2daa654466a03d
Ji Yi <xixixi_0987@hotmail.com>
2018-11-15 06:27:26 +0000

    1

--- python/E6B2-CWZ6C.py
+++ python/E6B2-CWZ6C.py
@@ -9,12 +9,12 @@ import RPi.GPIO
 from gpiozero import PWMLED
 
 #接受编码器针脚编号设置
-encoderOUTA=20
-encoderOUTB=21
+encoderOUTA=27
+encoderOUTB=17
 encoderOUTZ=26
 
 # LED0 指示灯 
-LED0=6
+LED0=10
 led0=PWMLED(LED0) 
 
 # setup GPIO triggered  记录按钮输入状态（是否按下）
@@ -30,16 +30,27 @@ RPi.GPIO.setup(encoderOUTA, RPi.GPIO.IN,pull_up_down=RPi.GPIO.PUD_DOWN)
 RPi.GPIO.setup(encoderOUTB, RPi.GPIO.IN,pull_up_down=RPi.GPIO.PUD_DOWN)
 RPi.GPIO.setup(encoderOUTZ, RPi.GPIO.IN,pull_up_down=RPi.GPIO.PUD_DOWN)
 
-#设置反馈方法
-def Arise(channel):   #按钮A反馈方法线程
-    global A_triggered
-    A_triggered= True
-    print("Afall")
+#设置输出针脚
+RPi.GPIO.setup(LED0,RPi.GPIO.OUT)
 
-def Afall(channel):   #按钮B反馈方法线程
+#设置反馈方法
+def SwitchA(channel):   
     global A_triggered
-    A_triggered= False
-    print("Afall")
+    if RPi.GPIO.input(encoderOUTA):
+        A_triggered= True
+        print("A == rise, A_triggered==True")
+    else:
+        A_triggered=False
+        print("A == fall, A_triggered==False")
+
+def SwitchB(channel):   
+    global B_triggered
+    if RPi.GPIO.input(encoderOUTB):
+        B_triggered= True
+        print("B == rise,B_triggered==True")
+    else:
+        B_triggered=False
+        print("B == fall,B_triggered==False")
 
 def SwitchZ(channel):
     global Z_triggered
@@ -55,55 +66,99 @@ def TriggeredAllFalse():  #将所有开关输入状态设置为否
 
 #设置事件检测
 # when a rising edge is detected on pin,the callback functions will be run
-RPi.GPIO.add_event_detect(encoderOUTA, RPi.GPIO.RISING,callback=Arise,bouncetime=200 ) # when use real Input swtich to switch the input level,
-RPi.GPIO.add_event_detect(encoderOUTB, RPi.GPIO.FALLING,callback=Afall,bouncetime=200 )
-RPi.GPIO.add_event_detect(encoderOUTB, RPi.GPIO.RISING,callback=SwitchB,bouncetime=200 ) # bouncetime=[time] to debounce the switch.
-RPi.GPIO.add_event_detect(encoderOUTZ, RPi.GPIO.RISING,callback=SwitchZ,bouncetime=200 )
+RPi.GPIO.add_event_detect(encoderOUTA, RPi.GPIO.BOTH,callback=SwitchA,bouncetime=5 ) # when use real Input swtich to switch the input level,
+RPi.GPIO.add_event_detect(encoderOUTB, RPi.GPIO.BOTH,callback=SwitchB,bouncetime=5 ) # when use real Input swtich to switch the input level,
+RPi.GPIO.add_event_detect(encoderOUTZ, RPi.GPIO.RISING,callback=SwitchZ,bouncetime=5 )
+
+#设定循环等待时间
+TIME=0.001
+
 
 if __name__ == "__main__":
     print("main start")
     led0.value=0
-    print("LED0の出力GPIOは：６")
-    print("Encoder Aの入力GPIOは：20")
-    print("Encoder Bの入力GPIOは：21")
+    print("LED0の出力GPIOは：10，順番で入力するとLED0の明るさを変えることができます")	
+    print("Encoder Aの入力GPIOは：27")
+    print("Encoder Bの入力GPIOは：17")
     print("Encoder Zの入力GPIOは：26")
     print("入力を待っている。。")
     try:
 # 主循环
         while True:
-            time.sleep(0.05)
- #第一次输入为A            
+            time.sleep(TIME)
+ #第一次输入为A升高            
             if A_triggered and B_triggered==False:
                 print("1番目の入力：A==Rising,B==0,2番目の入力を待っている。。。")
-                TriggeredAllFalse()
-    #第二次输入为B
+                # TriggeredAllFalse()
+    #第二次输入为B升高
                 while True:
-                        time.sleep(0.05)
-                        if B_triggered:
-                            if led0.value<=0.9:
-                                led0.value+=0.1
-                            elif led0.value==1:
-                                RPi.GPIO.output(LED0, True)
-                            print("2番目の入力：B==RIising,終わり。1番目の入力を待っている。。。")
-                            B_triggered=False
-                            A_triggered=False
-                            break
-  #第一次输入为B                      
-            if B_triggered and A_triggered==False:
-                TriggeredAllFalse()
-                print("1番目の入力：B==Rising,A==0,2番目の入力を待っている。。。")       
-    #第二次输入为A
-                while True:
-                    time.sleep(0.05)
-                    if A_triggered:
-                        if led0.value>=0.1:
-                            led0.value-=0.1
-                        elif led0.value==0:
-                            RPi.GPIO.output(LED0, False)
-                        print("2番目の入力：A==RIising,終わり。1番目の入力を待っている。。。")
-                        B_triggered=False
-                        A_triggered=False
-                        break    
+                    time.sleep(TIME)
+                    if B_triggered:
+                        print("入力2==B rise，入力3を待っている。。。")
+        #第三次输入为A下降
+                        while True:
+                            time.sleep(TIME)
+                            if A_triggered==False:
+                                print("入力3==A fall，入力4を待っている。。。")
+            #第四次输入为B下降
+                                while True:
+                                    time.sleep(TIME)
+                                    if B_triggered==False:
+                                        print("入力4==B fall。回転方向： CW ")
+                                        if led0.value<=0.9:
+                                            led0.value+=0.1
+                                        elif led0.value==1:
+                                            RPi.GPIO.output(LED0, True)
+                                        break #退出第四次循环
+                                break  #退出第三次循环
+        #第三次输入为B下降                                
+                            elif B_triggered==False:
+                                print("入力3==B fall")
+                                break #退出第三次循环
+                        print("入力1に戻る，入力1を待っている。。。")
+                        break #退出第二层循环           
+    #第二次输入为A下降
+                    elif A_triggered==False:
+                        print("入力1に戻る，入力1を待っている。。。")
+                        break #退出第二次循环
 
+
+  #第一次输入为B升高                      
+            if B_triggered and A_triggered==False:
+                print("1番目の入力：B==Rising,A==0,2番目の入力を待っている。。。")       
+    #第二次输入为A升高            
+                while True:
+                    time.sleep(TIME)
+                    if A_triggered:
+                        print("入力2==A rise，入力3を待っている。。。")
+        #第三次输入为B下降
+                        while True:
+                            time.sleep(TIME)
+                            if B_triggered==False:
+                                print("入力3==B fall，入力4を待っている。。。")
+            #第四次输入为A下降
+                                while True:
+                                    time.sleep(TIME)
+                                    if A_triggered==False:
+                                        print("入力4==A fall。回転方向： CWW ")
+                                        if led0.value>=0.1:
+                                               led0.value-=0.1
+                                        elif led0.value==0:
+                                            RPi.GPIO.output(LED0, False)
+                                        break #退出第四次循环
+                                break  #退出第三次循环
+        #第三次输入为A下降                                
+                            elif A_triggered==False:
+                                print("入力3==A fall")
+                                break #退出第三次循环
+                        print("入力1に戻る，入力1を待っている。。。")
+                        break #退出第二层循环           
+    #第二次输入为B下降
+                    elif B_triggered==False:
+                        print("入力2==B fall")
+                        print("入力1に戻る，入力1を待っている。。。")
+                        break #退出第二次循环
+
+                        
     finally:
         RPi.GPIO.cleanup()
--- python/test.py
+++ python/test.py
@@ -2,11 +2,9 @@ import time
 import datetime
 from gpiozero import PWMLED
 
-led = PWMLED(6)
-
+led = PWMLED(10)
 led.on()
-time.sleep(3)  # the led should be lit
-#led.off()  # the led should go off
-led.value = 0.5  # the led should be lit at half brightness
+time.sleep(3)
+led.off()
 while True:
 	pass
