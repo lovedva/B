0777a3967c0b8e313282760b0e231d0e672c202d
Ji.Y <xixixi_0987@hotmail.com>
2019-09-17 02:18:04 +0100

    1

--- python/pid90Ctr.py
+++ python/pid90Ctr.py
@@ -24,7 +24,7 @@ class PIinit:
 class pidCtr:
 	"PID控制器"
 	flag=1  
-	Sv=90.000 #用户输入
+	Sv=90.000 #用户输入设定的温度
 	Pv=0.000
 	T=500.000 #ms PID计算周期
 	Kp=20.000 #比例系数
--- python/test.py
+++ python/test.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import time
-import datetime
-from gpiozero import PWMLED
-import ina219
-import adt7410
+# import datetime
+# # from gpiozero import PWMLED
+# import ina219
+# import adt7410
+import RPi.GPIO
+RPi.GPIO.setmode(RPi.GPIO.BCM)
+RPi.GPIO.setup(5, RPi.GPIO.OUT)
 
-# led2 = PWMLED(12)
-# led2.on()
-# print ("gpio开启")
-
-
-while True:
-    time.sleep(0.5)
-    ina219.read()
-    print("")
-    print(adt7410.read_adt7410())
+try:
+    RPi.GPIO.output(5, True)
+    print ("gpio开启")
 
 
+    while True:
+        time.sleep(0.5)
+        # ina219.read()
+        print("111")
+        # print(adt7410.read_adt7410())
+
+finally:
+    RPi.GPIO.cleanup()
