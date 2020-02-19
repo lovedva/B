85634d5d68a65f22b8906057709f748d0e99eb5b
Ji.Y <xixixi_0987@hotmail.com>
2020-02-19 07:36:57 +0000

    1

--- python/pid60Ctr.py
+++ python/pid60Ctr.py
@@ -5,6 +5,7 @@ import RPi.GPIO
 import sys
 import adt7410
 import pt1000
+import max31855
 
 class PIinit:
     "初始化板子参数设定"
@@ -24,12 +25,12 @@ class PIinit:
 class pidCtr:
     "PID控制器"
     flag=1  
-    Sv=60.000 #用户输入
+    Sv=50.000 #用户输入
     Pv=0.000
-    T=750.000 #ms PID计算周期
-    Kp=5.000 #比例系数
+    T=0.2000 #ms PID计算周期
+    Kp=20.000 #比例系数
     Ti=50000.000 #ms 积分时间
-    Td=1000.000 #ms 微分时间
+    Td=125.000 #ms 微分时间
     Ek=0.000 #本次偏差
     Ek_1=0.000 #上次偏差
     SEk=0.000 #历史偏差总和
@@ -59,7 +60,8 @@ class pidCtr:
         Kd=self.Kp*td #微分系数
         self.Dout=Kd*DeltaEK #3-微分项输出
         # print("Dout==%s"%self.Dout)
-        out=self.Pout+self.Iout+self.Dout+self.OUT0 #4-pid计算结果
+        # out=self.Pout+self.Iout+self.Dout+self.OUT0 #4-pid计算结果
+        out=self.Pout
         # print("calc.out==%s"%out)
         #pid计算结果处理‘’
         if out>self.pwmcycle:
@@ -84,14 +86,15 @@ if __name__ == "__main__":
         pwm.start(1)
         file_handle=open('60Templog.txt',mode='w')
         while True:
-            time.sleep(0.5)
+            time.sleep(0.2)
+            pid.Pv=max31855.sensor.readTempC()
             # pid.Pv=adt7410.read_adt7410()
-            pid.Pv=float(pt1000.calcTemp(4,5))
-            print("Pt1000で測温[4,5]==%s"%pid.Pv)
-            sensortemp=adt7410.read_adt7410()
-            print("温度传感器测温==%s度"%sensortemp)
+            # pid.Pv=float(pt1000.calcTemp(4,5))
+            print("Pt1000で測温[4,5]==%s \n"%pid.Pv)
+            # sensortemp=adt7410.read_adt7410()
+            # print("温度传感器测温==%s度"%sensortemp)
             file_handle.write("%s | "%pid.Pv)
-            file_handle.write("%s \n"%sensortemp)
+            # file_handle.write("%s \n"%sensortemp)
             pid.calc()
             print("pidCr.OUTの計算結果==%s"%pid.OUT)
             dc=pid.OUT/pid.pwmcycle*100
